0e03de8ba0b31b3cd95a493805a697be786c9a66
Dimas Nasution <dimasfirmansyxx@gmail.com>
2022-11-26 09:22:19 +0700

    init

---
+++ app.py
@@ -0,0 +1,52 @@
+from flask import Flask, render_template,url_for,request,jsonify
+from flask_cors import CORS, cross_origin
+import joblib
+import os
+import pickle
+import numpy as np
+from tensorflow.keras.models import load_model
+from sklearn.preprocessing import StandardScaler
+
+sc = StandardScaler()
+
+app = Flask(__name__)
+CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/result',methods=['POST'])
+@cross_origin()
+def result():
+    cylinders = int(request.form['cylinders'])
+    displacement = int(request.form['displacement'])
+    horsepower = int(request.form['horsepower'])
+    weight = int(request.form['weight'])
+    model_year = int(request.form['model_year'])
+    
+    values = [[cylinders,displacement,horsepower,weight,model_year]]
+
+    scaler_path = os.path.join(os.path.dirname('models/'),'scaler.pkl')
+
+    sc = None
+    with open(scaler_path,'rb') as f:
+        sc=pickle.load(f)
+        
+    values = sc.transform(values)
+
+    model = load_model(r'models/model.h5')
+
+    prediction = model.predict(values)
+    prediction = float(prediction)
+    print(prediction)
+    
+    json_dict={
+        'prediction':prediction
+    }   
+    
+    return jsonify(json_dict)
+    
+if __name__=='__main__':
+    app.run(debug=True,port=3298)
